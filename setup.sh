1a952e1fc939fc007057de3a86f851c946dedb54
karansharmahr <63999241+karansharmahr@users.noreply.github.com>
2021-01-09 23:00:07 +0530

    setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,12 @@
 #! /bin/bash
 cd /data/data/com.termux/files/usr/bin
 rm -rf phish
+rm -rf details
 cd 
 cd Phishing
 cp -r details /data/data/com.termux/files/usr/bin
 cp -r phish /data/data/com.termux/files/usr/bin
 cd /data/data/com.termux/files/usr/bin
-chmod 765 details
-chmod 765 phish
 chmod 775 phish
 chmod 775 details
 cd

